e32cbca03ff2ddaccf86c58b58a76af184b8f116
Durga Prasad Keshri <keshridurgaprasad@gmail.com>
2026-02-28 17:02:05 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,25 +1,12 @@
--- Customers Table
-CREATE TABLE customers (
-    customer_id INT PRIMARY KEY,
-    customer_name VARCHAR(100),
-    city VARCHAR(50)
-);
-
--- Restaurants Table
-CREATE TABLE restaurants (
-    restaurant_id INT PRIMARY KEY,
-    restaurant_name VARCHAR(100),
-    city VARCHAR(50)
-);
-
--- Orders Table
-CREATE TABLE orders (
-    order_id INT PRIMARY KEY,
-    customer_id INT,
-    restaurant_id INT,
-    order_amount DECIMAL(10,2),
-    order_time TIMESTAMP,
-    delivery_time INT,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
-    FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id)
+CREATE TABLE food_delivery (
+    rest_id INT,
+    area TEXT,
+    city TEXT,
+    restaurant TEXT,
+    price INT,
+    avg_ratings DECIMAL(10,2),
+    total_ratings INT,
+    food_type TEXT,
+    address TEXT,
+    delivery_time INT
 );
